125e364b9828e4fab4d46cbcb682bc96457fdc93
msultanridhoridho-alt <msultanridhoridho@gmail.com>
2026-01-08 15:59:28 +0700

    Add files via upload

---
+++ django_ddp/tugas1/tugas1/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
